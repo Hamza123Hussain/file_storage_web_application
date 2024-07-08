d3402abeea0b11d1c2d2adbf1111b71d0c890c10
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-09 01:15:37 +0500

    Added Toasts

--- app/Folder/[ID]/page.jsx
+++ app/Folder/[ID]/page.jsx
@@ -1,16 +1,52 @@
+// pages/Folder.jsx
 'use client'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
 import { ParentIdContext } from '@/utils/Context'
-import React, { useContext, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js' // Import createClient from Supabase
+
+const supabaseUrl = 'https://msmetpsnmkznujcdtqub.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY // Replace with your Supabase anon key
+
+const supabase = createClient(supabaseUrl, supabaseKey) // Initialize Supabase client
 
 const Folder = ({ params }) => {
   const { setParentId } = useContext(ParentIdContext)
+  const [folderData, setFolderData] = useState(null)
+
+  const getFolderData = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('Folder')
+        .select('*')
+        .eq('parentID', params.ID) // Use params.ID directly here
+
+      if (error) {
+        console.error('Error fetching data from Supabase:', error.message)
+      } else {
+        console.log('Data fetched successfully:', data)
+        setFolderData(data) // Update state with fetched data
+      }
+    } catch (error) {
+      console.error('Unexpected error occurred:', error)
+    }
+  }
 
   useEffect(() => {
     setParentId(params.ID)
+    getFolderData()
   }, [])
+
   return (
     <div>
-      <h1>HEELO FOLDER {params.ID}</h1>
+      <h1>HELLO FOLDER {params.ID}</h1>
+      {/* Display folderData here */}
+      {folderData && (
+        <div>
+          <h2>Folder Name: {folderData.FolderName}</h2>
+          {/* Add more details as needed */}
+        </div>
+      )}
     </div>
   )
 }
---
+++ app/api/Folder/GetFolder/[ID]/route.js
@@ -0,0 +1,28 @@
+// pages/api/Folder/[id].js
+
+import { supabase } from '@/utils/Supabaseconfig'
+import { NextResponse } from 'next/server'
+
+export const GET = async (req) => {
+  try {
+    const { id } = req.query
+    const { data, error } = await supabase
+      .from('Folder')
+      .select('*')
+      .eq('parentID', id)
+
+    if (error) {
+      console.error('Error fetching data from Supabase:', error.message)
+      return NextResponse.json({ message: error.message }, { status: 500 })
+    }
+
+    console.log('Data fetched successfully:', data)
+    return NextResponse.json({ data }, { status: 200 })
+  } catch (error) {
+    console.error('Unexpected error occurred:', error)
+    return NextResponse.json(
+      { message: 'Unexpected error occurred' },
+      { status: 500 }
+    )
+  }
+}
