41b1db1b7361e6dc9f06d0bca8a8447c6999d2df
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-10 04:41:43 +0500

    Storage Update

--- components/Storage.jsx
+++ components/Storage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Storage = () => {
-  return <div>I AM STORAGE</div>
+  return (
+    <div className=" flex flex-col">
+      <h1>2 GB OUT OF 10 GB</h1>
+      <div className=" px-2 py-1 bg-green-300 border-2 text-transparent">
+        Storage Text
+      </div>
+    </div>
+  )
 }
 
 export default Storage
