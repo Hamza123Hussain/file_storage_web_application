6d381b599dd6d16a6cb2fae0055df126ebd5d0c5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-11 09:13:02 +0500

    Customized creation buttons

--- components/Files/CreateFile.jsx
+++ components/Files/CreateFile.jsx
@@ -4,6 +4,14 @@ import { fetchData } from '@/functions/FetchDataFiles'
 import { getFileType } from '@/functions/FileTypesShort'
 import { ParentIdContext } from '@/utils/Context'
 import React, { useContext, useEffect, useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 
 const CreateFileBTN = () => {
   const [File, setFileDetails] = useState({ Name: '', File: {}, FileType: '' })
@@ -60,86 +68,83 @@ const CreateFileBTN = () => {
   }, [filecreated])
   return (
     <div>
-      <button
-        className="w-48 flex gap-2 items-center bg-blue-600 hover:brightness-105 text-white rounded-lg p-3"
-        onClick={() => document.getElementById('my_modal_3').showModal()}
-      >
-        <span className="text-lg">Add A File</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
-      </button>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button
-              onClick={() => {
-                setFileDetails({ File: {}, Name: '' })
-              }}
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+      <Dialog>
+        <DialogTrigger>
+          {' '}
+          <button className="w-48 flex gap-2 items-center bg-blue-600 hover:brightness-105 text-white rounded-lg p-3">
+            <span className="text-lg">Add A File</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
             >
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Add A File</h3>
-          <div className="py-4 flex flex-col">
-            <input
-              value={File.Name}
-              name="Name"
-              onChange={ChangeValue}
-              type="text"
-              placeholder="Enter File Name"
-              className="p-3 w-full rounded-lg border-2 border-r-slate-100"
-            />
-            <div className="flex flex-col justify-center items-start gap-2">
-              <div className="relative w-full mt-4">
-                <label className="flex items-center hover:shadow-amber-900 hover:shadow-md justify-center w-full p-3 rounded-full bg-green-500 text-white cursor-pointer hover:bg-green-600 transition duration-300 ease-in-out">
-                  <span>Add A File You Want to Upload</span>
-                  <input
-                    name="File"
-                    type="file"
-                    onChange={ChangeValue}
-                    className="absolute inset-0 opacity-0 cursor-pointer"
-                    placeholder="Enter File"
-                  />
-                </label>
-              </div>
-              <div className="text-xs text-gray-400 capitalize">
-                Note : Only A File Upto 1GB can be Uploaded on This Website
-              </div>
-              {File?.File?.size > 0 ? (
-                <div className="flex justify-between gap-10 items-center text-xs sm:text-sm text-green-400 font-bold">
-                  <h3>FileType: {File.FileType}</h3>
-                  <h3>
-                    Size: {(File.File.size / (1024 * 1024)).toFixed(2)} MB
-                  </h3>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {' '}
+              <h3 className="font-bold text-lg">Add A File</h3>
+            </DialogTitle>
+            <DialogDescription>
+              <div className="py-4 flex flex-col">
+                <input
+                  value={File.Name}
+                  name="Name"
+                  onChange={ChangeValue}
+                  type="text"
+                  placeholder="Enter File Name"
+                  className="p-3 w-full rounded-lg border-2 border-r-slate-100"
+                />
+                <div className="flex flex-col justify-center items-start gap-2">
+                  <div className="relative w-full mt-4">
+                    <label className="flex items-center hover:shadow-amber-900 hover:shadow-md justify-center w-full p-3 rounded-full bg-green-500 text-white cursor-pointer hover:bg-green-600 transition duration-300 ease-in-out">
+                      <span>Add A File You Want to Upload</span>
+                      <input
+                        name="File"
+                        type="file"
+                        onChange={ChangeValue}
+                        className="absolute inset-0 opacity-0 cursor-pointer"
+                        placeholder="Enter File"
+                      />
+                    </label>
+                  </div>
+                  <div className="text-xs text-gray-400 capitalize">
+                    Note : Only A File Upto 1GB can be Uploaded on This Website
+                  </div>
+                  {File?.File?.size > 0 ? (
+                    <div className="flex justify-between gap-10 items-center text-xs sm:text-sm text-green-400 font-bold">
+                      <h3>FileType: {File.FileType}</h3>
+                      <h3>
+                        Size: {(File.File.size / (1024 * 1024)).toFixed(2)} MB
+                      </h3>
+                    </div>
+                  ) : (
+                    ''
+                  )}
                 </div>
-              ) : (
-                ''
-              )}
-            </div>
-            <div className="flex justify-end mt-2">
-              <button
-                onClick={AddFileTOSupabase}
-                className="px-3 py-1 hover:brightness-105 text-white bg-green-400 rounded-lg"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-      </dialog>
+                <div className="flex justify-end mt-2">
+                  <button
+                    onClick={AddFileTOSupabase}
+                    className="px-3 py-1 hover:brightness-105 text-white bg-green-400 rounded-lg"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
--- components/Folder/CreateFolderBtn.jsx
+++ components/Folder/CreateFolderBtn.jsx
@@ -1,27 +1,33 @@
 'use client'
-// components/CreateFolderBtn.js
-
 import { CreateFolder } from '@/functions/CreateFolder'
 import { ParentIdContext } from '@/utils/Context'
 import React, { useContext, useEffect, useState } from 'react'
 import FolderList from './FolderList'
 import { fetchFolderData } from '@/functions/Fetchfolderdata'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 
 const Folderbtn = () => {
   const { setFolderData, parentId } = useContext(ParentIdContext)
   const [foldername, setname] = useState('')
-
   const [FolderCreated, setCreated] = useState(false)
+
   const handleCreateFolder = async () => {
     try {
       await CreateFolder(foldername, parentId)
       setCreated(true)
     } catch (error) {
-      // Handle error cases
       console.error('Error creating folder:', error)
       alert('Failed to create folder')
     }
   }
+
   const GetFolderData = async () => {
     const data = await fetchFolderData()
     setFolderData(data)
@@ -36,54 +42,53 @@ const Folderbtn = () => {
 
   return (
     <div>
-      <button
-        className="w-48 flex gap-2 items-center bg-green-600 hover:brightness-105 text-white rounded-lg p-3"
-        onClick={() => document.getElementById('my_modal_1').showModal()}
-      >
-        <span className="text-lg">Add A Folder</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
-      </button>
-
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Add A Folder</h3>
-          <div className="py-4 flex flex-col">
-            <input
-              value={foldername}
-              onChange={(e) => setname(e.target.value)}
-              type="text"
-              placeholder="Enter Folder Name"
-              className="p-3 w-full rounded-lg border-2 border-r-slate-100"
-            />
-            <div className="flex justify-end mt-2">
-              <button
-                onClick={handleCreateFolder}
-                className="px-3 py-1 hover:brightness-105 text-white bg-green-400 rounded-lg"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-      </dialog>
+      <Dialog>
+        <DialogTrigger>
+          <button className="w-48 flex gap-2 items-center bg-green-600 hover:brightness-105 text-white rounded-lg p-3">
+            <span className="text-lg">Add A Folder</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <h3 className="font-bold text-lg">Add A Folder</h3>
+            </DialogTitle>
+            <DialogDescription>
+              <div className="py-4 flex flex-col">
+                <input
+                  value={foldername}
+                  onChange={(e) => setname(e.target.value)}
+                  type="text"
+                  placeholder="Enter Folder Name"
+                  className="p-3 w-full rounded-lg border-2 border-r-slate-100"
+                />
+                <div className="flex justify-end mt-2">
+                  <button
+                    onClick={handleCreateFolder}
+                    className="px-3 py-1 hover:brightness-105 text-white bg-green-400 rounded-lg"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
